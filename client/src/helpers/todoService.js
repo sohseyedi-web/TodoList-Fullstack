14f8940ad26fd011f4a8f7bab0b302df0c55f7c5
sohseyedi <sohseyedi@gmail.com>
2023-06-20 21:58:42 +0330

    todoservice helper

--- client/src/helpers/todoService.js
+++ client/src/helpers/todoService.js
@@ -4,3 +4,25 @@ export const getTodos = async () => {
   const { data } = await axios.get("http://localhost:4000/tasks/all");
   return data;
 };
+export const saveTodos = async (data) => {
+  const { data } = await axios.post(
+    "http://localhost:4000/tasks",
+    { ...data },
+    { withCredentials: true }
+  );
+  return data;
+};
+export const updateTodos = async (id, data) => {
+  const { data } = await axios.put(
+    `http://localhost:4000/tasks/update/${id}`,
+    { ...data },
+    { withCredentials: true }
+  );
+  return data;
+};
+export const deleteTodos = async (id) => {
+  const { data } = await axios.delete(`http://localhost:4000/tasks/${id}`, {
+    withCredentials: true,
+  });
+  return data;
+};
